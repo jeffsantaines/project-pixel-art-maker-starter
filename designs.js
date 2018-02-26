36653c1a7e193b5bc5c594b6b93de44babf66278
jeffsantaines <javacodegod@gmail.com>
2018-02-27 07:42:18 +1300

    Variables added

--- designs.js
+++ designs.js
@@ -1,5 +1,12 @@
+/*
+
+*/
+
 // Select color input
+const color = document.getElementById('colorPicker').value;
 // Select size input
+const height = document.getElementById('inputHeight').value;
+const weight = document.getElementById('inputWidht').value;
 
 // When size is submitted by the user, call makeGrid()
 
